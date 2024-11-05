2cce31e67cd6e0c0564459f76a70987f81fa114b
JoseOtero15 <joseotero1504@gmail.com>
2024-11-05 10:33:43 -0500

    Crud functions added

--- src/main/java/com/h2example/h2/repositories/TaskRepository.java
+++ src/main/java/com/h2example/h2/repositories/TaskRepository.java
@@ -3,5 +3,5 @@ package com.h2example.h2.repositories;
 import com.h2example.h2.models.Task;
 import org.springframework.data.repository.CrudRepository;
 
-public interface InterfaceTaskDao extends CrudRepository<Task, Long> {
+public interface TaskRepository extends CrudRepository<Task, Long> {
 }
--- src/main/java/com/h2example/h2/rest/ToDoList.java
+++ src/main/java/com/h2example/h2/rest/ToDoList.java
@@ -4,6 +4,7 @@ import com.h2example.h2.models.Task;
 import com.h2example.h2.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 import java.util.List;
 
 @RestController
@@ -15,10 +16,32 @@ public class ToDoList {
 
     @GetMapping(value="tasks")
     public List<Task> getTasks() {
-        return taskService.getTask();
+        return taskService.getTasks();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 
+    @PostMapping("/create")
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        Task updatedTask = taskService.updateTask(id, taskDetails);
+        return ResponseEntity.ok(updatedTask);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
 
